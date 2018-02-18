8b404706755ef0f5854ac4e00baccf0838043f09
Carolina <carolina@Carolinas-MacBook-Air.local>
2018-02-17 20:04:18 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+ //Comment 
+ let myNumber; 
+ const PI = 3.14; 
+
+ let colots = [ 'red', 'green', 'blue', 'white'. 'purple']; //array: a list of string
+
+ const navBar = document.getElementById('nav-bar');
+ navBar.style.background = 'red';
+
+ function changeColor
+
+ function addTwoNumber(a, b) {
+     return a+b;
+ }
